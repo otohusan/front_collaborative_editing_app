ee3502999741b1cc614f85cafdaa37aae437d42f
otohusan <tamioguitar@gmail.com>
2024-01-12 21:35:05 +0900

    APIを叩いてuserを取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import getUserData from "./api/getUserData";
+import { User } from "../type/user.type";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userData, setUserData] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getUserData("1"); // 例: ユーザーID 1 のデータを取得
+      setUserData(data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {userData ? (
+        <div>
+          <h1>{userData.user_name}</h1>
+          <p>{userData.hobby}</p>
+          {/* その他のユーザー情報を表示 */}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
